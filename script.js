1a5ed7a14ce6389c08a76bc01be71c455a5dc35e
Nkosingiphile Mduduzi Hlongwane <87560589+MzilaSoftSolutions@users.noreply.github.com>
2024-09-22 05:54:33 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function scrollToSection(sectionId) {
+    document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
+}
+Example
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+
+function openNav() {
+  document.getElementById("myNav").style.height = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.height = "0%";
+}
+
